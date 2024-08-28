9f02f5bb47079acef72b32582abad7f33ed6aa96
Sazib <mahiburislamsazib@gmail.com>
2024-08-28 14:29:05 +0600

    string

---
+++ string.js
@@ -0,0 +1,34 @@
+const name1 = 'mahibur' + ' ' + 'islam';
+//console.log(name1);
+
+
+const firstName = 'mahibur'
+const lastName = 'islam'
+const age = '23'
+const incom = '2'
+
+//regular system 
+const all = 'this is' + ' ' + firstName + ' ' + lastName + ' ' + 'my age' +' ' + age + '  ' + ' ' + 'my earn' +' ' + incom;
+//console.log(all);
+
+
+// advance system
+const system = `this is ${firstName} ${lastName}. my age ${age}. my earn ${incom}`
+//console.log(system);
+
+
+// used \n to line down step by step
+console.log(
+    'this is sazib. \ni am a student . \ni study college'
+);
+
+// `` use this to alsow down line
+console.log(
+    `this is sazib. i am a student . i study college`
+);
+
+
+
+
+
+
