edb37ac8f8389457a3555db21440ad8f3464bc56
Sazib <mahiburislamsazib@gmail.com>
2024-08-28 13:49:46 +0600

    const updet

--- var-let-const.js
+++ var-let-const.js
@@ -2,4 +2,14 @@
 
 let sazib = 24 ; // update variable to used let
 sazib = 34;
-console.log(sazib);
+//console.log(sazib);
+
+const jannat = 34;
+const asif = jannat+6;// diclared variable to update const  
+
+//console.log(asif);
+
+
+
+
+
