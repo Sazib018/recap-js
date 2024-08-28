28920aebeffadef1b5eb9594184fa95a820ba72b
Sazib <mahiburislamsazib@gmail.com>
2024-08-28 13:53:50 +0600

    array

--- var-let-const.js
+++ var-let-const.js
@@ -9,6 +9,14 @@ const asif = jannat+6;// diclared variable to update const
 
 //console.log(asif);
 
+// const used all number of array 
+const numbers = [23 , 33 , 44 , 21 ,55 ,66];
+
+for (let i = 0; i < numbers.length; i++) {
+    const num = numbers[i];
+    console.log(num);
+    
+}
 
 
 
